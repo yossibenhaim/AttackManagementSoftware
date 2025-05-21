6b301484dbc4827016f3b364cda9e5a7f571652c
Shlominoyshtat <s0583244814@gmail.com>
2025-05-21 15:49:51 +0300

    sekend add the weapons to function

--- attack_management_software/Weapons.cs
+++ attack_management_software/Weapons.cs
@@ -1,4 +1,25 @@
-﻿public class Weapon : IAtteckTool
+﻿public class f16jet : IAtteckTool
 {
+    public string name { get; set; }
+    public string purpose { get; set; }
+    public int countOfAattack { get; set; }
+    public int countOfGas { get; set; }
+
+    public f16jet(string name, string purpose, int countOfAattack, int countOfGas)
+    {
+        Name = name;
+        Purpose = purpose;
+        CountOfAttack = countOfAattack;
+        CountOfGas = countOfGas;
+    }
+    public void changeCountAttack()
+    {
+        CountOfAttack --;
+    }
+    public void changeCountGas()
+    {
+        CountOfGas -= 5;
+    }
+
 
 }
