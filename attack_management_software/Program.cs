a9a904d7d92b22ba68c335101e704ff66a97718e
Yossi Ben Haim <v0585204770@gmail.com>
2025-05-21 09:36:24 +0300

    Add project files.

---
+++ attack_management_software/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace attack_management_software
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
