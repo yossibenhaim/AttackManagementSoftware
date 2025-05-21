b7b4b3649a68de22ee6f25eba17d4da9353f69b4
Yossi Ben Haim <v0585204770@gmail.com>
2025-05-21 11:48:30 +0300

    added the interface Iattacktool

---
+++ attack_management_software/IAttackTool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace attack_management_software
+{
+    internal interface IAttackTool
+    {
+        string name { get; set; }
+        string purpose { get; set; }
+        int countOfAattack { get; set; }
+        int countOfGas { get; set; }
+
+        void IAttackTool(string name, string purpose, int countOfAattack, int countOfGas);
+
+        void changeCountAttack();
+        void changeCountGas();
+
+    }
+}
