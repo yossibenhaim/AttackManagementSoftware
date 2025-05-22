77780251f710c27e25f0b9f4b129d0314b453687
Yossi Ben Haim <v0585204770@gmail.com>
2025-05-22 08:47:32 +0300

    added the class IDF foll and class attack empty

---
+++ attack_management_software/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace attack_management_software
+{
+    internal class IDF
+    {
+        private IAttackTool weapons;
+        private IInfomition infomition;
+        private Attack attack;
+
+        public IDF(IAttackTool attackTool, IInfomition infomition, Attack attack)
+        {this.weapons = attackTool; this.infomition = infomition; this.attack = attack; }
+
+    }
+}
---
+++ attack_management_software/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace attack_management_software
+{
+    internal class Attack
+    {
+    }
+}
